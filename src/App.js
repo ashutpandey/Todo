9e80f43bb19a8a179685a1a2913296ee3c6c2919
ashutpandey <ashutoshpandeydeos834@gmail.com>
2025-09-14 17:25:18 +0530

    good for commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import WelcomePage from './pages/WelcomePage';
 import TodoPage from './pages/TodoPage';
@@ -7,18 +7,27 @@ import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import './App.css';
 
+// Create a parent layout component
+const MainLayout = () => (
+    <>
+        <Navbar />
+        <main>
+            <Outlet />
+        </main>
+    </>
+);
+
 function App() {
     return (
         <Router>
-            <Navbar />
-            <main>
-                <Routes>
-                    <Route path="/" element={<WelcomePage />} />
-                    <Route path="/todos" element={<TodoPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                </Routes>
-            </main>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<WelcomePage />} />
+                    <Route path="todos" element={<TodoPage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route path="contact" element={<ContactPage />} />
+                </Route>
+            </Routes>
         </Router>
     );
 }
